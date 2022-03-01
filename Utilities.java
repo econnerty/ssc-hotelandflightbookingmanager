87925fe61ab7af473359b260458ccc57cf868667
Erik Connerty <erikc@email.sc.edu>
2022-03-01 17:23:04 -0500

    Setting some stuff up

---
+++ Utilities.java
@@ -0,0 +1,38 @@
+import java.util.*;
+import java.security.*;
+import java.io.*;
+
+import org.json.simple.*;
+import org.json.simple.parser.JSONParser;
+
+
+
+public class Utilities {
+
+    private static final String USER_JSON_PATH = "data/users.json";
+    private static final String PLANE_JSON_PATH = "data/planes.json";
+    private static final String HOTEL_JSON_PATH = "data/hotels.json";
+
+    private static MessageDigest md;
+    private static JSONParser JSONParser;
+
+    private static Utilities utilities;
+
+    private Utilities() {
+
+        try { md = MessageDigest.getInstance("SHA-256");} 
+        catch (NoSuchAlgorithmException e) {e.printStackTrace();}
+
+        JSONParser = new JSONParser();
+    }
+
+    public static Utilities getInstance() {
+        
+        if (utilities == null)
+            Utilities.utilities = new Utilities();
+        
+        return Utilities.utilities;
+        
+    }
+    
+}
