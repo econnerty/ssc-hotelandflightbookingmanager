b393a1f93c4daab00a856a51d83283ae8f2567a0
Erik Connerty <erikc@email.sc.edu>
2022-03-01 20:17:38 -0500

    Implement singleton and implement manager

--- java/UserManager.java
+++ java/UserManager.java
@@ -1,5 +1,21 @@
 package java;
 
-public class UserManager {
+public class UserManager implements Manager {
+
+    private static UserManager userManager;
+
+    private UserManager(){
+
+    }
+
+    public static UserManager getInstance() {
+
+        if (userManager == null)
+            userManager = new UserManager();
+
+        return userManager;
+        
+
+    }
     
 }
