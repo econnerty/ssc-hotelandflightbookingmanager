f90706c57f3f9d415ec40e45caf698b937caf873
Erik Connerty <erikc@email.sc.edu>
2022-03-01 21:25:48 -0500

    Setting some stuff up

--- src/ApplicationManager.java
+++ src/ApplicationManager.java
@@ -3,9 +3,12 @@ package src;
 public class ApplicationManager implements Manager{
 
     private static ApplicationManager appManager;
+    private static UserManager userManager;
+    private static BookingManager bookingManager;
 
     private ApplicationManager(){
-
+        userManager = UserManager.getInstance();
+        bookingManager = BookingManager.getInstance();
     }
 
     public static ApplicationManager getInstance() {
