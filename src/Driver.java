65fdf4e1b2853c876345cb3e4dcb99de77a3063d
Erik Connerty <erikc@email.sc.edu>
2022-03-02 18:59:23 -0500

    Trying to use tuples instead of Bookings class. Added library

--- src/Driver.java
+++ src/Driver.java
@@ -1,10 +1,23 @@
 package src;
 
 public class Driver {
+
+    private static String currentUser = "";
+
+    private static boolean run(){
+
+        return false;
+    }
     
     public static void main(String args[]) {
 
         ApplicationManager app = ApplicationManager.getInstance();
+
+        while(run() == true);
+
+        
+
+        
             
     }
 
--- src/Users.java
+++ src/Users.java
@@ -3,11 +3,13 @@ package src;
 import java.util.ArrayList;
 import java.util.Date;
 
+import org.javatuples.*;
+
 public abstract class Users {
 
     protected String name;
     protected Date date;
-    protected ArrayList<Booking> bookings;
+    protected  Pair<String, int[]> bookings;
 
     public class RegisteredUser extends Users {
 
