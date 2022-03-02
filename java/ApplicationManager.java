db16d7c51d2fe2e17e8cbcd5136618a20b60099d
Erik Connerty <erikc@email.sc.edu>
2022-03-01 20:03:37 -0500

    Convert Manager to Singleton

--- java/ApplicationManager.java
+++ java/ApplicationManager.java
@@ -1,5 +1,21 @@
 package java;
 
 public class ApplicationManager {
+
+    private static ApplicationManager appManager;
+
+    private ApplicationManager(){
+
+    }
+
+    public ApplicationManager getInstance() {
+
+        if (appManager == null)
+            appManager = new ApplicationManager();
+
+        return appManager;
+        
+
+    }
     
 }
--- java/BookingManager.java
+++ java/BookingManager.java
@@ -1,5 +1,21 @@
 package java;
 
 public class BookingManager {
+
+    private static BookingManager bookingManager;
+
+    private BookingManager(){
+
+    }
+
+    public BookingManager getInstance() {
+
+        if (bookingManager == null)
+            bookingManager = new BookingManager();
+
+        return bookingManager;
+        
+
+    }
     
 }
