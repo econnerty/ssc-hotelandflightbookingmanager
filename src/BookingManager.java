c9c1319eceb41cf4e7a89835e8ef1bbde8f1e4e8
Erik Connerty <erikc@email.sc.edu>
2022-03-02 19:07:23 -0500

    Convert arrays to hashmaps

--- src/BookingManager.java
+++ src/BookingManager.java
@@ -1,13 +1,14 @@
 package src;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class BookingManager implements Manager{
 
     private static BookingManager bookingManager;
 
-    private static ArrayList<Plane> planes; //We will store all the planes in this arraylist
-    private static ArrayList<Hotel> hotels; //We will store all the hotels in this arraylist
+    private static HashMap<String,Plane> planes; //We will store all the planes in this hashmap. The uuid will be the key.
+    private static HashMap<String,Hotel> hotels; //We will store all the hotels in this hashmap. The uuid will be the key.
 
 
     private BookingManager(){
--- src/UserManager.java
+++ src/UserManager.java
@@ -1,12 +1,13 @@
 package src;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class UserManager implements Manager {
 
     private static UserManager userManager;
     private static Users currentUser;
-    private static ArrayList<Users> users;
+    private static HashMap<String, Users> users; //We will store all the users inside this hashmap. The UUID will be the key.
 
     private UserManager(){
 
