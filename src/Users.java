3b9e92919f9422bbaab5ef3816ad279d2499077c
Erik Connerty <erikc@email.sc.edu>
2022-03-02 19:03:28 -0500

    Comment

--- src/Users.java
+++ src/Users.java
@@ -8,7 +8,7 @@ import org.javatuples.*;
 public abstract class Users {
 
     protected String name;
-    protected Date date;
+    protected Date creationDate; //Date the user was created
     protected Pair<String, int[]> bookings; //Bookings can just be a tuple in users. The string will be the uuid to the plane or hotel. The int array will hold the coordinates to the room or the plane's seat
 
     public class RegisteredUser extends Users {
