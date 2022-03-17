c48917396dded4b8c6800be03b07f1550a464685
maemaja <maja@email.sc.edu>
2022-03-17 12:10:27 -0400

    deleting classes from users

--- src/Users.java
+++ src/Users.java
@@ -11,14 +11,5 @@ public abstract class Users {
     protected String password; //This should be the hashed password if we have time for that
     protected Date creationDate; //Date the user was created
     protected Pair<String, int[]> bookings; //Bookings can just be a tuple in users. The string will be the uuid to the plane or hotel. The int array will hold the coordinates to the room or the plane's seat
-
-    public class RegisteredUser extends Users {
-
-
-    }
-
-    public class BusinessUser extends Users {
-        
-    }
     
 }
