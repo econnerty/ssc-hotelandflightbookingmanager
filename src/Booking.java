20ffdfd0259fa93f33c38c9d34a165ef836c5e4b
Erik Connerty <erikc@email.sc.edu>
2022-03-18 12:48:21 -0400

    what

--- src/Booking.java
+++ src/Booking.java
@@ -4,13 +4,19 @@ import java.util.UUID;
 
 public abstract class Booking {
     protected UUID uuid;
+    protected int[] index;
 
     public Booking(UUID uuid){
         this.uuid = uuid;
     }
 
+    public Booking(UUID uuid, int[] index){
+        this.uuid = uuid;
+        this.index = index;
+    }
+
     public String toString() {
-        return this.uuid.toString();
+        return this.uuid.toString() + index.toString();
     }
     
     
