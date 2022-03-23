8e968de1f4ec1ea2f3f109e9ecc7737a48e7d991
Erik Connerty <erikc@email.sc.edu>
2022-03-22 22:16:01 -0400

    working account sign up

--- src/Driver.java
+++ src/Driver.java
@@ -25,23 +25,23 @@ public class Driver {
     private static boolean run() throws java.text.ParseException, IOException{
 
         if (currentUser.getClass() == GuestUser.class)
-            mainMenu();
+            return mainMenu();
         else if (currentUser.getClass() == BusinessUser.class)
-            businessMainMenu();
+            return businessMainMenu();
         else if (currentUser.getClass() == RegisteredUser.class)
-            registeredMainMenu();
+            return registeredMainMenu();
 
 
 
         return true;
     }
 
-    public static void mainMenu() throws IOException, java.text.ParseException {
+    public static boolean mainMenu() throws IOException, java.text.ParseException {
         //cheapest flights
 
         System.out.println(currentUser.menuString());
 
-        int action = Integer.parseInt(input.nextLine())6;
+        int action = Integer.parseInt(input.nextLine());
 
 
         switch (action) {
@@ -61,7 +61,8 @@ public class Driver {
                 int userType = Integer.parseInt(input.nextLine());
 
 
-                String output = (appManager.signUp(username, password, dob, types[userType-1])) ? "Sign Up Succesful!" : "Something went wrong";
+                System.out.println( (appManager.signUp(username, password, dob, types[userType-1])) ? "Sign Up Succesful!\n" : "Something went wrong\n");
+                currentUser = appManager.getCurrentUser();
                 break;
             case 2:
                 break;
@@ -76,13 +77,15 @@ public class Driver {
                 break;
             case 7:
                 appManager.close();
+                return false;
             default:
                 break;
         }
+        return true;
     }
 
 
-    public static void registeredMainMenu() {
+    public static boolean registeredMainMenu() {
         
         //cheapest flights
 
@@ -102,9 +105,10 @@ public class Driver {
             default:
                 break;
         }
+        return true;
     }
 
-    public static void businessMainMenu() {
+    public static boolean businessMainMenu() {
         Scanner input = new Scanner(System.in);
         //cheapest flights
 
@@ -124,6 +128,7 @@ public class Driver {
             default:
                 break;
         }
+        return true;
     }
     
     public static void main(String args[]) throws FileNotFoundException, IOException, ParseException, java.text.ParseException, NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
