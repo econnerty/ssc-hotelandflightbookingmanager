c1d96b506f69087b444b02157ea8648bcfe820c7
maemaja <maja@email.sc.edu>
2022-03-27 19:16:26 -0400

    amenities enums

---
+++ src/Amenities.java
@@ -0,0 +1,20 @@
+package src;
+
+public enum Amenities {
+    FREE_WIFI,
+    ROOM_SERVICE,
+    POOL,
+    FREE_BREAKFAST,
+    MINI_FRIDGE,
+    24_HOUR_GUEST_RECEPTION,
+    LAUNDRY_SERVICES,
+    SPA,
+    SAUNA,
+    GYM,
+    BATHROBES,
+    BAR,
+    HAIR_TOOLS,
+    MICROWAVE,
+    CLOTHING_IRON,
+    ON_SITE_PARKING
+}
