f83c5179fd06865cf0a0172d4ceeca32d03db8fa
Shaine M <shaine@email.sc.edu>
2022-03-29 13:34:53 -0400

    added uuid & business.json reader to Utilities and updated Business.java.

--- src/Business.java
+++ src/Business.java
@@ -2,20 +2,20 @@ package src;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.UUID;
 
 public class Business implements src.JSON {
 
+    private UUID uuid;
     private String name;
     private double rating;
     private int numRatings;
 
-    public Business(String name, double rating, int numRatings) {
+    public Business(UUID uuid, String name, double rating, int numRatings) {
+        this.uuid = uuid;
         this.name = name;
         this.rating = rating;
         this.numRatings = numRatings;//should start as zero in the implementing method for a new Business.
-    }
-    public void getBookings() { //stub from UML
-
     }
     public double addRating(String name, Double newRating) {
         if(rating > 5 || rating <0) 
@@ -24,14 +24,34 @@ public class Business implements src.JSON {
         this.numRatings++;
         return this.rating;
     }
+    public void setRating(UUID uuid, String name, Double rating, int numRatings) {
+        if(rating <= 5 || rating >=0) {
+            this.uuid = uuid;
+            this.name = name;
+            this.rating = rating;
+            this.numRatings = numRatings;
+        }
+    }
+    public UUID getUUID() {
+        return this.uuid;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return this.name;
+    }
     public double getRating() {
         return this.rating;
     }
-
+    public int getnumRatings() {
+        return this.numRatings;
+    }
     public Map toJsonObject() {
         
         Map jsonObject = new LinkedHashMap<>();
 
+        jsonObject.put("UUID",this.uuid);
         jsonObject.put("name",this.name);
         jsonObject.put("numRatings", this.numRatings);
         jsonObject.put("rating",this.rating);
