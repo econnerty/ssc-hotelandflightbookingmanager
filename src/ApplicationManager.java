de18d8778eae548eefeb5d9f2302291d38e189ff
maemaja <maja@email.sc.edu>
2022-03-30 22:04:08 -0400

    appmanager javadoc

--- src/ApplicationManager.java
+++ src/ApplicationManager.java
@@ -10,6 +10,10 @@ import java.util.Scanner;
 
 import org.json.simple.parser.ParseException;
 
+/**
+ * Application Manager class manages the whole application
+ */
+
 public class ApplicationManager {
 
     private static ApplicationManager appManager;
@@ -17,13 +21,27 @@ public class ApplicationManager {
     private static BookingManager bookingManager;
 
 
-
+    /**
+     * private constructor
+     * @throws FileNotFoundException
+     * @throws IOException
+     * @throws ParseException
+     * @throws java.text.ParseException
+     */
     private ApplicationManager() throws FileNotFoundException, IOException, ParseException, java.text.ParseException{
         Utilities.getInstance();
         userManager = UserManager.getInstance();
         bookingManager = BookingManager.getInstance();
     }
 
+    /**
+     * gets instance of Application Manager class
+     * @return
+     * @throws FileNotFoundException
+     * @throws IOException
+     * @throws ParseException
+     * @throws java.text.ParseException
+     */
     public static ApplicationManager getInstance() throws FileNotFoundException, IOException, ParseException, java.text.ParseException {
 
         if (appManager == null)
@@ -33,6 +51,15 @@ public class ApplicationManager {
         
     }
 
+    /**
+     * signs up a new User
+     * @param username
+     * @param password
+     * @param dob
+     * @param type
+     * @return
+     * @throws IOException
+     */
     public boolean signUp(String username, String password, Date dob, String type) throws IOException{
 
         User user;
@@ -51,10 +78,18 @@ public class ApplicationManager {
         
     }
 
+    /**
+     * logs out current user
+     * @throws IOException
+     */
     public void logout() throws IOException {
         userManager.logout();
     }
 
+    /**
+     * closes application
+     * @throws IOException
+     */
     public void close() throws IOException{
         Utilities.saveHotels(bookingManager.getHotels()); //Uncomment this when to json object is complete
         Utilities.savePlanes(bookingManager.getPlanes());
@@ -62,15 +97,28 @@ public class ApplicationManager {
     }
 
 
+    /**
+     * searches for flights based on parameters
+     * @param departure city to be departed from
+     * @param destination place to be transported to
+     */
     public void searchFlights(String departure, String destination){
         bookingManager.searchFlights(departure, destination);
     }
 
-
+    /**
+     * searches for hotels based on parameters
+     * @param search destination for which the hotel will be
+     */
     public void searchHotels(String search){
         bookingManager.searchHotels(search);   
     }
 
+    /**
+     * books flights
+     * @param choice number of booking
+     * @return boolean value to see if the choice exists
+     */
     public boolean bookFlight(int choice) {
         try {
             return bookingManager.bookFlight(choice);
@@ -81,6 +129,11 @@ public class ApplicationManager {
         return false;
     }
 
+    /**
+     * books hotels
+     * @param choice number of booking
+     * @return boolean value to see if the choice exists
+     */
     public boolean bookHotel(int choice) {
         try {
             return bookingManager.bookHotel(choice);
@@ -91,17 +144,21 @@ public class ApplicationManager {
         return false;
     }
 
+    /**
+     * logs existing user in
+     * @param username user name
+     * @param password password
+     * @return boolean value on whether or not the user exists
+     */
     public boolean login(String username, String password) {
         return (userManager.login(username, password));
     }
 
+    /**
+     * returns current user
+     * @return the User currently on the application
+     */
     public User getCurrentUser() {
         return userManager.getCurrentUser();
-    }
-
-    public boolean print() {
-        //TODO
-        return false;
-    }
-    
+    }    
 }
