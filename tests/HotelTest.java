e055581b03b9bc2b65542224089e950519bbd004
Erik Connerty <erikc@email.sc.edu>
2022-04-10 17:33:22 -0400

    refactor hoteltest

---
+++ tests/HotelTest.java
@@ -0,0 +1,32 @@
+package tests;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import src.Hotel;
+
+class HotelTest {
+    private Hotel hotel;
+
+    @BeforeEach
+    public void setup() {
+        
+    }
+
+    @AfterEach
+    public void tearDown() {
+
+    }
+
+    @Test
+    void testValidHotels() {
+
+    }
+
+    @Test
+    void testRoomBooking() {
+
+    }
+}
