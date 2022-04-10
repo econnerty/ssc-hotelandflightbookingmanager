f7767e90bac3d04b9c85aae0963c23c340ae2f24
Erik Connerty <erikc@email.sc.edu>
2022-04-10 14:26:37 -0400

    testing loading json

---
+++ src/UtilitiesTest.java
@@ -0,0 +1,46 @@
+package src;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.UUID;
+
+import org.json.simple.parser.ParseException;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class UtilitiesTest {
+    private Utilities utils;
+    private HashMap<UUID, Plane> planes;
+    private HashMap<UUID, Hotels> hotels;
+    private HashMap<String, User> users;
+
+    @BeforeEach
+    public void oneTimesetup() {
+        utils = Utilities.getInstance();
+    }
+
+    @AfterEach
+    public void tearDown() {
+
+    }
+
+    @Test
+    void testLoadPlanes() throws FileNotFoundException, IOException, ParseException, java.text.ParseException {
+        planes = utils.loadPlanes();
+
+        assertNotNull(planes);
+        assertEquals(504, planes.size()); //There are 504 planes in our json file
+    }
+
+    @Test
+    void testLoadUsers() {
+
+    }
+
+
+
+    
+}
