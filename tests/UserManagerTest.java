cdb9d81b9ea4c7ddf3d2326eafe7ad4197d974ec
luke93257 <69948520+luke93257@users.noreply.github.com>
2022-04-11 00:59:20 -0400

    Testing

---
+++ tests/UserManagerTest.java
@@ -0,0 +1,52 @@
+package tests;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import org.junit.BeforeClass;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class UserManagerTest {
+
+	private static UserManager userManager;
+    private static User currentUser;
+	
+	 private static HashMap<String, User> users; //We will store all the users inside this hashmap. The UUID will be the key.
+
+	@BeforeEach
+	public void setup() throws FileNotFoundException, IOException, ParseException, java.text.ParseException {
+		Utilities.getInstance();
+		userManager = UserManager.getInstance();
+		
+		
+	}
+	
+	@Test
+	public void testLogin() {
+		userManager.login("tom","1234" );
+	}
+	
+	@Test
+	public void testChangePassword() {
+		userManager.changePassword("1234","4321");
+		
+	}
+	 @Test
+	    public void testLogout() throws IOException {
+	        assertTrue(userManager.login("tom","1234"));
+	        userManager.logout();
+	        assertEquals(userManager.getCurrentUser().getUsername(), "Guest");
+	    }
+	 @Test
+	 	public void testaddUser() throws IOException{
+		 userManager.addUser("bob", currentUser);
+		 assertEquals("bob", userManager.getUsers());
+	 }
+	
+}
