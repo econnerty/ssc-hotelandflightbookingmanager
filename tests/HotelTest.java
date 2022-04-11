5188bb1771610d2c84eb512d41b703650d1fb697
maemaja <maja@email.sc.edu>
2022-04-10 20:42:55 -0400

    hotel testing

--- tests/HotelTest.java
+++ tests/HotelTest.java
@@ -1,3 +1,6 @@
+/**
+ * author: Maeko Maja
+ */
 package tests;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -6,13 +9,14 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import src.Hotel;
+import src.Hotels;
 
 class HotelTest {
     private Hotel hotel;
 
     @BeforeEach
     public void setup() {
-        
+        hotel = new Hotel(null, 300, Hotels.MARRIOTT, 45.67, "Brooklyn", true, true, 3.4);
     }
 
     @AfterEach
@@ -22,11 +26,7 @@ class HotelTest {
 
     @Test
     void testValidHotels() {
-
-    }
-
-    @Test
-    void testRoomBooking() {
-
+        hotel = new Hotel(null, 0, null, 0, null, true, true, 0);
+        assertEquals("Doesn't exist", hotel.getHotelInfo());
     }
 }
